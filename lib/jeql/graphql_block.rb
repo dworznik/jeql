74a975fe3b7d04b5c0c1fade0171125c11f7dd68
Patryk Dwórznik <patryk@dworznik.com>
2019-09-10 12:42:20 +0900

    Improve var handling
     - Resolve vars in the query tag attributes
     - Results in the {{endpoint}}_{{var}} variable
     - Render query files as templates

--- lib/jeql/graphql_block.rb
+++ lib/jeql/graphql_block.rb
@@ -1,20 +1,30 @@
 class Jeql::GraphqlBlock < Liquid::Block
   GraphQlError = Class.new(Jekyll::Errors::FatalException)
-  PARAMS_SYNTAX = /(\w+):\s*['"](\w+)['"],?/
+  LITERALS_SYNTAX = /(\w+):\s*['"](\w+)['"],?/
+  VARS_SYNTAX = /(\w+):\s*(\w+)\s*,?/
 
   def initialize(tag_name, text, tokens)
     super
-    @params = text.scan(PARAMS_SYNTAX)
+    @literals = Hash[text.scan(LITERALS_SYNTAX)]
+    @vars = Hash[text.scan(VARS_SYNTAX)]
     @text = text
   end
 
-  def render(context)
-    hash_params = Hash[@params]
+  def get_value(context, name)
+    if @literals.has_key?(name)
+      return @literals[name]
+    elsif @vars.has_key?(name)
+      return context[name]
+    end
+  end
 
-    endpoint_config = context.registers[:site].config["jeql"][hash_params["endpoint"]]
-    query = Jeql::Query.new(hash_params["query"], context.registers[:site].config["source"], endpoint_config)
+  def render(context)
+    endpoint_config = context.registers[:site].config["jeql"][self.get_value(context, "endpoint")]
+    query = Jeql::Query.new(self.get_value(context, "query"), context.registers[:site].config["source"], endpoint_config, context)
+    var_name = self.get_value(context, "endpoint") + "_" + (self.get_value(context, "var") || "data")
+    print var_name
     if query.response.success?
-      context['data'] = JSON.parse(query.response.body)['data']
+      context[var_name] = JSON.parse(query.response.body)['data']
       super
     else
       raise GraphQlError, "The query #{query.query_name} failed"
